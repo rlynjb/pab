f6f9fff56512836ad39edb539ca0a000faa68047
RLyn Ben <rlyn@brydansolutions.com>
2015-08-31 10:12:33 -0700

    fix for user data

--- app/app.js
+++ app/app.js
@@ -29,19 +29,16 @@ var headerView = Backbone.View.extend({
   el: '#header-inner',
   template: _.template( $('#header-inner-content').html() ),
   initialize: function() {
-    if (isUserLoggedIn) {
-      var u = localStorage.getItem('firebase:session::pab');
-      var parseU = JSON.parse(u);
-      this.userInfo = {
-        email: parseU.password.email
-      }
-    }
-
     this.render();
   },
   render: function() {
     if (isUserLoggedIn) {
-      var tplContent = this.template(this.userInfo);
+      var u = localStorage.getItem('firebase:session::pab');
+      var parseU = JSON.parse(u);
+      var userInfo = {
+        email: parseU.password.email
+      }
+      var tplContent = this.template(userInfo);
       this.$el.html( tplContent );
     } else {
       this.$el.html( this.template(this) );
