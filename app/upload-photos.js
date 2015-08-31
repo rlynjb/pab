a66a3e5f2cad24d3fd0b4668a934d4a95415f33e
RLyn Ben <rlyn@brydansolutions.com>
2015-08-31 16:39:29 -0700

    task to do in comment

--- app/upload-photos.js
+++ app/upload-photos.js
@@ -1,3 +1,41 @@
+/*
+ * we are coding a complex Backbone Model
+ * this is the model class's code
+ * */
+var Photo = Backbone.Model.extend({
+  urlRoot: '/photos',
+  /*
+   * NOTE:
+   * all models & collections uses Backbone.sync method
+   * sync is the method thats called everytime were
+   * reading or writing one or more models to or from the server
+   * - Backbone doesnt support AJAX file uploads, that is why
+   *   we have to overwrite sync function in model level only
+   * - This is the method that is used for reading, updating, 
+   *   and deleting this model's instance.
+   * - It takes in 3 parameters
+   *   1st, CRUD method (create, read, update, delete)
+   *   2nd, model instance
+   *   3rd, options object
+   * */
+  sync: function(method, model, options) {
+    /*
+     * TODO:
+     * play around with this sync function
+     * we need options
+     * this is where we convert image file to data url
+     * https://github.com/firebase/firepano
+     * */
+    console.log('1st param', method);
+    console.log('2nd param', model);
+    console.log('3rd param', options);
+  }
+});
+
+var Photos = Backbone.Collection.extend({
+  model: Photo
+});
+
 var uploadPhotoPageView = Backbone.View.extend({
   id: 'upload-photo-page',
   template: _.template( $('#upload-photo-page').html() ),
@@ -16,6 +54,7 @@ var addPhotoView = Backbone.View.extend({
   template: _.template( $('#add-photo-form-content').html() ),
   initialize: function(options) {
     // we are passing Photos Collection as a initialize param
+    // rather then instantiating new Photos Collection object
     this.photos = this.options
     this.render();
   },
@@ -34,7 +73,16 @@ var addPhotoView = Backbone.View.extend({
       file: $('#imageUpload')[0].files[0],
       caption: $('#imageCaption').val()
     });
+    /*
+     * TODO
+     * find a way to console log Model sync method
+     * */
+    console.log(photo);
     // create new photo by calling Photos Collection create method
+    // TODO uncomment when ready
     this.photos.create(photo, { wait: true });
+
+    // clears the form
+    //this.el.reset();
   }
 });
