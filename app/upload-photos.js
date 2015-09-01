07abb1182145b5556407c82a430dc97773ed2a61
RLyn Ben <rlyn@brydansolutions.com>
2015-09-01 14:21:42 -0700

    setup for converting img to base64 string

--- app/upload-photos.js
+++ app/upload-photos.js
@@ -1,40 +1,6 @@
-/*
- * we are coding a complex Backbone Model
- * this is the model class's code
- * */
 var Photo = Backbone.Model.extend({
-  // this is temp url, for testing
   urlRoot: apiUrl + '/photos.json'
-  /*
-   * NOTE:
-   * - Backbone doesnt support AJAX file uploads, that is why
-   *   we have to overwrite sync function in model level only
-   * - This is the method that is used for reading, updating, 
-   *   and deleting this model's instance.
-   * - It takes in 3 parameters
-   *   1st, CRUD method (create, read, update, delete)
-   *   2nd, model instance
-   *   3rd, options object
-   * */
-  //sync: function(method, model, options) {
-    /*
-     * TODO:
-     * play around with this sync function
-     * we need options
-     * this is where we convert image file to data url
-     * https://github.com/firebase/firepano
-     * */
-    //console.log('crud method', method);
-    //console.log('model passed on', model);
-    //console.log('options', options);
-  //}
 });
-
-/*
- * Testing purpose
- * */
-var fg = new Photo();
-console.log(fg);
 
 var Photos = Backbone.Collection.extend({
   model: Photo
@@ -81,12 +47,13 @@ var addPhotoView = Backbone.View.extend({
       file: $('#imageUpload')[0].files[0],
       caption: $('#imageCaption').val()
     });
+
     /*
-     * TODO
-     * find a way to console log Model sync method
+     * Convert image file to base64 string and post to firebase
      * */
+
     // create new photo by calling Photos Collection create method
-    this.photos.create(photo, { wait: true });
+    //this.photos.create(photo, { wait: true });
 
     // clears the form
     this.el.reset();
